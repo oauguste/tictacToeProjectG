c130319b0298cbed71e6d24e90aaa984dac669ed
oauguste <augusteosnac@gmail.com>
2023-02-26 01:50:13 -0500

    Project files init
    some of the javascript is completed, working board

---
+++ script.js
@@ -0,0 +1,40 @@
+let origBoard;
+const huPlayer = "O";
+const aiPlayer = "X";
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+const cells = document.querySelectorAll(".cell");
+
+startGame();
+
+function startGame() {
+  document.querySelector(
+    ".endgame"
+  ).getElementsByClassName.display = "none";
+
+  origBoard = Array.from(Array(9).keys());
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].textContent = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
+}
+
+function turnClick(square) {
+  turn(square.target.id, huPlayer);
+}
+
+function turn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+}
